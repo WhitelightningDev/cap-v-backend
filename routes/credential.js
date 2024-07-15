f9455dd7a4434b50c6f41de37122af0e4e6ef11e
WhitelightningDev <danielmommsen@hotmail.com>
2024-07-15 23:42:07 +0200

    "Refactor credential routes to use credentialController"

--- routes/credential.js
+++ routes/credential.js
@@ -1,45 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const Credential = require('../models/Credential');
-const authMiddleware = require('../middleware/authMiddleware'); // Import authentication middleware
-const config = require('../config/config');
+const credentialController = require('../controllers/credentialController');
 
-router.use(express.json());
+// Get all credentials
+router.get('/', credentialController.getCredentials);
 
-// Fetch all credentials (Unprotected route)
-router.get('/', async (req, res) => {
-  try {
-    // Fetch all credentials and populate the 'division' field with its 'name'
-    const credentials = await Credential.find().populate('division', 'name');
-    res.json(credentials);
-  } catch (error) {
-    console.error('Error fetching credentials:', error.message);
-    res.status(500).json({ msg: 'Server Error' });
-  }
-});
+// Create a new credential
+router.post('/', credentialController.createCredential);
 
-// Add new credential (Protected route)
-router.post('/', authMiddleware, async (req, res) => {
-  const { title, username, password, division } = req.body;
+// Update a credential by ID
+router.put('/:id', credentialController.updateCredential);
 
-  try {
-    // Create a new credential object
-    const newCredential = new Credential({
-      title,
-      username,
-      password,
-      division,
-    });
-
-    // Save the new credential to the database
-    await newCredential.save();
-
-    // Return the newly created credential in the response
-    res.status(201).json(newCredential);
-  } catch (error) {
-    console.error('Error adding credential:', error.message);
-    res.status(500).json({ msg: 'Server Error' });
-  }
-});
+// Delete a credential by ID
+router.delete('/:id', credentialController.deleteCredential);
 
 module.exports = router;
